e014927fc8fe0734c773836d93ae78a278cc03f1
unknown <ciechacki.dariusz@gmail.com>
2024-02-29 22:19:30 +0100

    added driver, webbrowser type, config loader and test case class

---
+++ src/test/java/library/TestCase.java
@@ -0,0 +1,36 @@
+package library;
+
+import com.demoqa.config.ConfigLoader;
+import drivers.BrowserType;
+import org.openqa.selenium.Dimension;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import static drivers.Driver.driver;
+
+public class TestCase {
+
+    @BeforeTest
+    public static void setUpWebDriver() {
+        String browserName = System.getProperty("browser", "CHROME");
+
+        BrowserType browserType = BrowserType.valueOf(browserName.toUpperCase());
+        driver = browserType.createWebDriver();
+
+        setWindowDimension();
+        navigateToBasePageUrl();
+    }
+
+    @AfterTest
+    public static void tearDown() {
+        driver.quit();
+    }
+
+    private static void setWindowDimension(){
+        driver.manage().window().setSize(new Dimension(1700, 1080));
+    }
+
+    private static void navigateToBasePageUrl(){
+        driver.get(ConfigLoader.getProperty("baseUrl"));
+    }
+}
