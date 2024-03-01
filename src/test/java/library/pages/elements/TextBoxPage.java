f1b566118bc7290e26f6295a27ebac58d61db8fc
unknown <ciechacki.dariusz@gmail.com>
2024-03-01 21:20:07 +0100

    added getting form data from output

--- src/test/java/library/pages/elements/TextBoxPage.java
+++ src/test/java/library/pages/elements/TextBoxPage.java
@@ -1,5 +1,7 @@
 package library.pages.elements;
 
+import library.modules.forms.FormsDto;
+import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,6 +10,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static drivers.Driver.driver;
 
@@ -18,6 +23,24 @@ public class TextBoxPage {
 
     @FindBy(xpath = "//h1[text()='Text Box']")
     WebElement textBoxPageTitle;
+
+    @FindBy(xpath = "//input[@id='userName']")
+    WebElement userName;
+
+    @FindBy(xpath = "//input[@id='userEmail']")
+    WebElement userEmail;
+
+    @FindBy(xpath = "//textarea[@id='currentAddress']")
+    WebElement currentAddress;
+
+    @FindBy(xpath = "//textarea[@id='permanentAddress']")
+    WebElement permanentAddress;
+
+    @FindBy(id = "submit")
+    WebElement submitButton;
+
+    @FindBy(id = "output")
+    WebElement formOutputElement;
 
     public boolean waitForTextBoxPageContent(){
         try {
@@ -31,5 +54,45 @@ public class TextBoxPage {
         return true;
     }
 
+    public void fillTextBoxForm(FormsDto formsDto){
+        userName.sendKeys(formsDto.getFullName());
+        userEmail.sendKeys(formsDto.getEmail());
+        currentAddress.sendKeys(formsDto.getCurrentAddress());
+        permanentAddress.sendKeys(formsDto.getPermanentAddress());
+    }
 
+    public Map<String, String> getFilledFormData(){
+        Map<String, String> formData = new HashMap<>();
+
+        formData.put("name", userName.getAttribute("value"));
+        formData.put("email", userEmail.getAttribute("value"));
+        formData.put("currentAddress", currentAddress.getAttribute("value"));
+        formData.put("permanentAddress", permanentAddress.getAttribute("value"));
+
+        return formData;
+    }
+
+    public void submitForm(){
+        submitButton.click();
+        waitForFormOutput();
+    }
+
+    public Map<String, String> getOutputFormData(){
+        List<WebElement> outputFormElements = formOutputElement.findElements(By.xpath("//p[@id]"));
+        Map<String, String> outputFormData = new HashMap<>();
+
+        for (WebElement outputFormElement : outputFormElements){
+            String outputFormValue = outputFormElement.getText();
+            int lastIndex = outputFormValue.lastIndexOf(":");
+
+            outputFormData.put(outputFormElement.getAttribute("id"), outputFormValue.substring(lastIndex + 1));
+        }
+
+        return outputFormData;
+    }
+
+    private void waitForFormOutput(){
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions
+                .elementToBeClickable(formOutputElement));
+    }
 }
--- src/test/java/tests/elements/FillTextBoxFormTest.java
+++ src/test/java/tests/elements/FillTextBoxFormTest.java
@@ -2,10 +2,13 @@ package tests.elements;
 
 import library.TestCase;
 import library.modules.SidebarMenu;
+import library.modules.forms.FormsFactory;
 import library.pages.HomePage;
 import library.pages.elements.TextBoxPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Map;
 
 public class FillTextBoxFormTest extends TestCase {
 
@@ -21,6 +24,11 @@ public class FillTextBoxFormTest extends TestCase {
         boolean textBoxPageVisible = textBoxPage.waitForTextBoxPageContent();
         Assert.assertTrue(textBoxPageVisible, "Text box page is not visible");
 
+        textBoxPage.fillTextBoxForm(FormsFactory.createFormsData());
+        Map<String, String> formData = textBoxPage.getFilledFormData();
 
+        textBoxPage.submitForm();
+        Assert.assertEquals(textBoxPage.getOutputFormData(), formData,
+                "Incorrect form data");
     }
 }
