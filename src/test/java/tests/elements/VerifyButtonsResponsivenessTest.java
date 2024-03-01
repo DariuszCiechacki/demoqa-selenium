b868291e191302dd2811cca9f5ed36a30b3400f4
unknown <ciechacki.dariusz@gmail.com>
2024-03-01 23:05:32 +0100

    deleted unnecessary isDisplayed

--- src/test/java/tests/elements/VerifyButtonsResponsivenessTest.java
+++ src/test/java/tests/elements/VerifyButtonsResponsivenessTest.java
@@ -1,13 +1,13 @@
 package tests.elements;
 
+import library.TestCase;
 import library.modules.common.SidebarMenu;
 import library.pages.HomePage;
 import library.pages.elements.ButtonsPage;
-import library.pages.elements.TextBoxPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class VerifyButtonsResponsivenessTest {
+public class VerifyButtonsResponsivenessTest extends TestCase {
     @Test(description = "Verify double click, right click and dynamic click buttons test")
     public void verifyButtonsResponsivenessTest() {
         boolean homePageVisible = new HomePage().waitForHomePageContent();
@@ -20,6 +20,8 @@ public class VerifyButtonsResponsivenessTest {
         boolean buttonsPageVisible = buttonsPage.waitForButtonsPageContent();
         Assert.assertTrue(buttonsPageVisible, "Buttons page is not visible");
 
-
+        Assert.assertTrue(buttonsPage.verifyDoubleClickButton(), "Double click was not performed successfully");
+        Assert.assertTrue(buttonsPage.verifyRightClickButton(), "Right click was not performed successfully");
+        Assert.assertTrue(buttonsPage.verifyDynamicClickButton(), "Dynamic click was not performed successfully");
     }
 }
