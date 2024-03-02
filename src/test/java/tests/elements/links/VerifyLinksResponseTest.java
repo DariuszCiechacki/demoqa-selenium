9716ba583bee21a2d730556bf04542802951fa3a
unknown <ciechacki.dariusz@gmail.com>
2024-03-02 18:41:11 +0100

    added test class

---
+++ src/test/java/tests/elements/links/VerifyLinksResponseTest.java
@@ -0,0 +1,12 @@
+package tests.elements.links;
+
+import library.TestCase;
+import org.testng.annotations.Test;
+
+public class VerifyLinksResponseTest extends TestCase {
+
+    @Test
+    public void verifyLinksResponseTest(){
+
+    }
+}
