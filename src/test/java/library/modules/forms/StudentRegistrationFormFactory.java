8a6b3e6a299cc6eb92301f79c03d24828af29f62
unknown <ciechacki.dariusz@gmail.com>
2024-03-04 21:16:41 +0100

    added student registration form dto

---
+++ src/test/java/library/modules/forms/StudentRegistrationFormFactory.java
@@ -0,0 +1,40 @@
+package library.modules.forms;
+
+import com.github.javafaker.Faker;
+
+import static library.modules.common.DefaultData.defaultEmail;
+
+public class StudentRegistrationFormFactory {
+    public static StudentRegistrationFormDto createEntireStudentRegistrationFormData(String gender, String dateOfBirth,
+                                                                                     String subjects, String hobbies,
+                                                                                     String state, String city){
+        StudentRegistrationFormDto studentRegistrationFormData = new StudentRegistrationFormDto();
+        Faker faker = new Faker();
+
+        studentRegistrationFormData.setFirstName(faker.name().firstName());
+        studentRegistrationFormData.setLastName(faker.name().lastName());
+        studentRegistrationFormData.setEmail(defaultEmail);
+        studentRegistrationFormData.setGender(gender);
+        studentRegistrationFormData.setMobileNumber(faker.phoneNumber().phoneNumber());
+        studentRegistrationFormData.setDateOfBirth(dateOfBirth);
+        studentRegistrationFormData.setSubjects(subjects);
+        studentRegistrationFormData.setHobbies(hobbies);
+        studentRegistrationFormData.setCurrentAddress(faker.address().fullAddress());
+        studentRegistrationFormData.setState(state);
+        studentRegistrationFormData.setCity(city);
+
+        return studentRegistrationFormData;
+    }
+
+    public static StudentRegistrationFormDto createRequiredStudentRegistrationFormData(String gender){
+        StudentRegistrationFormDto studentRegistrationFormData = new StudentRegistrationFormDto();
+        Faker faker = new Faker();
+
+        studentRegistrationFormData.setFirstName(faker.name().firstName());
+        studentRegistrationFormData.setLastName(faker.name().lastName());
+        studentRegistrationFormData.setGender(gender);
+        studentRegistrationFormData.setMobileNumber(faker.phoneNumber().subscriberNumber(10));
+
+        return studentRegistrationFormData;
+    }
+}
