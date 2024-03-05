ee2ba2b3af13050986042e150db9d271137e25d4
unknown <ciechacki.dariusz@gmail.com>
2024-03-05 22:03:59 +0100

    added get selected radiobutton label

--- src/test/java/library/modules/common/adapters/CheckboxesHandler.java
+++ src/test/java/library/modules/common/adapters/CheckboxesHandler.java
@@ -3,17 +3,46 @@ package library.modules.common.adapters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static drivers.Driver.driver;
 
 public class CheckboxesHandler {
-    public static void setCheckboxes(boolean state, String... values){
-        for (String value : values){
-            WebElement checkbox = driver.findElement(By.xpath("//label[contains(@for,'checkbox') and text()='"+value+"']"));
+    public static void setCheckboxes(boolean state, String... values) {
+        for (String value : values) {
+            WebElement checkbox = driver.findElement(By.xpath("//label[contains(@for,'checkbox') and text()='" + value + "']"));
 
             if (checkbox.findElement(By.xpath(".//preceding-sibling::input"))
-                    .isSelected() != state){
+                    .isSelected() != state) {
                 WebDriverActions.scrollToElementAndClick(checkbox);
             }
         }
+    }
+
+    public static String getSelectedCheckboxesLabelsList() {
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//label[contains(@for,'checkbox')]//preceding-sibling::input"));
+        List<String> checkedCheckboxesLabels = new ArrayList<>();
+
+        checkboxes.forEach(checkbox -> {
+            if (checkbox.isSelected()) {
+                checkedCheckboxesLabels.add(checkbox.findElement(By.xpath(".//following-sibling::label")).getText());
+            }
+        });
+
+        return String.join(", ", checkedCheckboxesLabels);
+    }
+
+    public static String getSelectedRadiobuttonLabel() {
+        List<WebElement> radios = driver.findElements(By.xpath("//label[contains(@for,'radio')]//preceding-sibling::input"));
+
+        for (WebElement radio : radios){
+            boolean radioState = radio.isSelected();
+            if (radioState){
+                return radio.findElement(By.xpath(".//following-sibling::label")).getText();
+            }
+        }
+
+        return null;
     }
 }
--- src/test/java/tests/forms/FillEntireStudentRegistrationFormTest.java
+++ src/test/java/tests/forms/FillEntireStudentRegistrationFormTest.java
@@ -28,10 +28,10 @@ public class FillEntireStudentRegistrationFormTest extends TestCase {
                 "Male", "20.9.1993", new String[]{"Math", "Physics"},
                 new String[]{"Reading", "Sports"}, "NCR", "Delhi"));
 
-        /*Map<String, String> studentRegistrationFormData = practiceFormPage.getStudentRegistrationFormData();
+        Map<String, String> studentRegistrationFormData = practiceFormPage.getStudentRegistrationFormData();
         System.out.println(studentRegistrationFormData);
 
-        SubmittedFormModal submittedFormModal = practiceFormPage.submitForm();
+        /*SubmittedFormModal submittedFormModal = practiceFormPage.submitForm();
 
         boolean submittedFormModalVisible = submittedFormModal.waitForSubmittedFormModalContent();
         Assert.assertTrue(submittedFormModalVisible, "Submitted form modal is not visible");
