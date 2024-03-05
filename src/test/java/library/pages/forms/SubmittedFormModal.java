7a8464259f669046b93a71096b7bfc424090c7c1
unknown <ciechacki.dariusz@gmail.com>
2024-03-05 21:21:15 +0100

    added split full name and state and city

--- src/test/java/library/pages/forms/SubmittedFormModal.java
+++ src/test/java/library/pages/forms/SubmittedFormModal.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static drivers.Driver.driver;
@@ -27,29 +28,37 @@ public class SubmittedFormModal {
     public Map<String, String> getSubmittedFormData(){
         Map<String, String> submittedFormData = new HashMap<>();
 
-        submittedFormData.put("firstName", driver.findElement(By.xpath("//td[text()='Student Name']//following-sibling::td"))
-                .getText());
-        submittedFormData.put("lastName", driver.findElement(By.xpath("//td[text()='Student Name']//following-sibling::td"))
-                .getText());
+        submittedFormData.put("firstName", splitFullName()[0]);
+        submittedFormData.put("lastName", splitFullName()[1]);
         submittedFormData.put("email", driver.findElement(By.xpath("//td[text()='Student Mail']//following-sibling::td"))
                 .getText());
         submittedFormData.put("gender", driver.findElement(By.xpath("//td[text()='Gender']//following-sibling::td"))
                 .getText());
         submittedFormData.put("mobileNumber", driver.findElement(By.xpath("//td[text()='Mobile']//following-sibling::td"))
                 .getText());
-        submittedFormData.put("dateOfBirth", driver.findElement(By.xpath("//td[text()='Date of Birth']//following-sibling::td"))
-                .getText());
+        /*submittedFormData.put("dateOfBirth", driver.findElement(By.xpath("//td[text()='Date of Birth']//following-sibling::td"))
+                .getText());*/
         submittedFormData.put("subjects", driver.findElement(By.xpath("//td[text()='Subjects']//following-sibling::td"))
                 .getText());
         submittedFormData.put("hobbies", driver.findElement(By.xpath("//td[text()='Hobbies']//following-sibling::td"))
                 .getText());
         submittedFormData.put("currentAddress", driver.findElement(By.xpath("//td[text()='Address']//following-sibling::td"))
                 .getText());
-        submittedFormData.put("state", driver.findElement(By.xpath("//td[text()='State and City']//following-sibling::td"))
-                .getText());
-        submittedFormData.put("city", driver.findElement(By.xpath("//td[text()='State and City']//following-sibling::td"))
-                .getText());
+        submittedFormData.put("state", splitStateAndCity()[0]);
+        submittedFormData.put("city", splitStateAndCity()[1]);
 
         return submittedFormData;
+    }
+
+    private String[] splitFullName(){
+        String fullName = driver.findElement(By.xpath("//td[text()='Student Name']//following-sibling::td"))
+                .getText();
+        return fullName.split(" ");
+    }
+
+    private String[] splitStateAndCity(){
+        String stateAndCity = driver.findElement(By.xpath("//td[text()='State and City']//following-sibling::td"))
+                .getText();
+        return stateAndCity.split(" ");
     }
 }
