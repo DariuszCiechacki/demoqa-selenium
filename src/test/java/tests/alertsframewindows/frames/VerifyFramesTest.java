ef9071bf6e1fcfcb0205fbc09dc487f251096a9c
unknown <ciechacki.dariusz@gmail.com>
2024-03-09 17:34:26 +0100

    add verify frames test

---
+++ src/test/java/tests/alertsframewindows/frames/VerifyFramesTest.java
@@ -0,0 +1,31 @@
+package tests.alertsframewindows.frames;
+
+import library.TestCase;
+import library.modules.common.SidebarMenu;
+import library.pages.HomePage;
+import library.pages.frames.FramesPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyFramesTest extends TestCase {
+    private final String expectedFrameText = "This is a sample page";
+
+    @Test
+    public void verifyFramesTest(){
+        boolean homePageVisible = new HomePage().waitForHomePageContent();
+        Assert.assertTrue(homePageVisible,"Home page is not visible");
+
+        new HomePage().navigateToAlertsFrameAndWindowsCategory();
+
+        FramesPage framesPage = new SidebarMenu().navigateToFramesPage();
+
+        boolean framesPageVisible = framesPage.waitForFramesPageContent();
+        Assert.assertTrue(framesPageVisible, "Frames page is not visible");
+
+        Assert.assertEquals(framesPage.getTextFromFirstFrame(), expectedFrameText,
+                "Incorrect first frame text");
+
+        Assert.assertEquals(framesPage.getTextFromSecondFrame(), expectedFrameText,
+                "Incorrect second frame text");
+    }
+}
